4879c26cb7241f131750f00c4af3253da81ae47c
Tagiyevff <162901971+Ramin12390@users.noreply.github.com>
2026-04-18 10:38:59 +0400

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
     SUPABASE_URL: 'https://jjshindwfumsdgclspws.supabase.co',
-    SUPABASE_ANON_KEY: 'sb_publishable_EjrM2ABfBqDb-ilDY9V79Q_1NSbCyTH',
+    SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Impqc2hpbmR3ZnVtc2RnY2xzcHdzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY0NjM5NzAsImV4cCI6MjA5MjAzOTk3MH0.LewvWaER4yDUCOhho6sEwotIp-BGpZhjxjfKB1pIJUY',
     BASE_EARNING: 0.008,
     UPGRADE_MULTIPLIER: 0.002,
     ADSGRAM_BLOCK_ID: '28035', // Replaced with actual block id if you have it
